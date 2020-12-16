9b570f1604c61e774bf85dd777984906c3c9b11d
Universum <programming@universegame.de>
2020-12-16 10:42:57 +0100

    updated gitignore

--- compiled/dataHandler/definitions.js
+++
@@ -1,6 +0,0 @@
-export const updateHTMLEvent = new Event('updateHTMLList');
-export const pinnedIndex = 0;
-export const pinnedFolderID = "pinned";
-export const unorderedIndex = Number.MAX_SAFE_INTEGER;
-export const unorderedFolderID = "unordered";
-//# sourceMappingURL=definitions.js.map
--- compiled/dataHandler/importer.js
+++
@@ -1,8 +0,0 @@
-export * from './adder.js';
-export * from './changer.js';
-export * from './checker.js';
-export * from './definitions.js';
-export * from './getter.js';
-export * from './updater.js';
-export * from './exporter.js';
-//# sourceMappingURL=importer.js.map
